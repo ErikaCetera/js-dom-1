be37b2ee294568de618dde7d8f6bf8e4bdcb5b0b
ErikaCetera <ceteraerika@gmail.com>
2024-11-07 15:28:03 +0100

    feat: bonus

--- js/script.js
+++ js/script.js
@@ -4,7 +4,23 @@ console.log(imgElement);
 const btnElement = document.getElementById("status");
 console.log(btnElement);
 
+// Prima del click isOn è false
+let lightOn = false; 
+
+//  Aspetta che l'utente faccia click sul bottone, una volta cliccato applica questa funzione
 btnElement.addEventListener("click", function() {
+    // Dopo il primo click; inverte isOn a true
+    if( lightOn= !lightOn){
      imgElement.src = "././img/yellow_lamp.png"; 
-     imgElement.alt ="Lampadina Accesa"
-})
+     imgElement.alt ="Lampadina Accesa";
+     btnElement.innerHTML = ("Spegni");
+    //  Dopo il secondo click: isOn = !isOn; inverte isOn a false
+   } else{
+    imgElement.src = "././img/white_lamp.png"; 
+     imgElement.alt ="Lampadina Spenta";
+     btnElement.innerHTML = ("Accendi");
+   }
+    
+}) 
+
+
